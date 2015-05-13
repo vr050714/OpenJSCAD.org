6731940e89f39439483b5e429c4ba5ca196f432b
Vladimir <romadanov@gmail.com>
2015-05-14 00:57:35 +0300

    bugfixing

--- stdlib.js
+++ stdlib.js
@@ -391,8 +391,8 @@ module.std = {
     var Line = function(prm) {
         if (typeof(prm)==='undefined') throw new Error('parameters are undefined');
         if ('p1' in prm && 'p2' in prm) {
-            if (typeof(prm['p1']) !== 'object' || length(prm['p1']) != 2) throw new Error('p1 is not 1x2 element array');
-            if (typeof(prm['p2']) !== 'object' || length(prm['p2']) != 2) throw new Error('p2 is not 1x2 element array');
+            if (typeof(prm['p1']) !== 'object' || prm['p1'].length != 2) throw new Error('p1 is not 1x2 element array');
+            if (typeof(prm['p2']) !== 'object' || prm['p2'].length != 2) throw new Error('p2 is not 1x2 element array');
             var p1 = prm['p1'].slice(0);
             var p2 = prm['p2'].slice(0);
             var tx = p2[0]-p1[0];
@@ -401,7 +401,7 @@ module.std = {
             this.norm = [-ty/tn, tx/tn];
             this.dist = this.norm[0]*p1[0]+this.norm[1]*p1[1];
         } else if ('norm' in prm && 'dist' in prm) {
-            if (typeof(prm['norm']) !== 'object' || length(prm['norm']) != 2) throw new Error('norm is not 1x2 element array');
+            if (typeof(prm['norm']) !== 'object' || prm['norm'].length != 2) throw new Error('norm is not 1x2 element array');
             if (typeof(prm['dist']) !== 'number') throw new Error('dist is not a number');
             this.norm = prm['norm'].slice(0);
             this.dist = prm['dist'];
@@ -421,19 +421,19 @@ module.std = {
     };
 
     Line.prototype.distance = function(p) {
-        if (typeof(p) !== 'object' || length(p) != 2) throw new Error('p is not 1x2 element array');
+        if (typeof(p) !== 'object' || p.length != 2) throw new Error('p is not 1x2 element array');
         return this.norm[0]*p[0]+this.norm[1]*p[1]-this.dist;
     };
 
     Line.prototype.projection = function(p) {
-        if (typeof(p) !== 'object' && length(p) != 2) throw new Error('p is not 1x2 element array');
+        if (typeof(p) !== 'object' && p.length != 2) throw new Error('p is not 1x2 element array');
         var s = this.norm[0]*p[0]+this.norm[1]*p[1]-this.dist;
         return [p[0]-s*this.norm[0], p[1]-s*this.norm[1]];
     };
 
     var Segment = function(p1, p2) {
-        if (typeof(p1) !== 'object' || length(p1) != 2) throw new Error('p1 is not 1x2 element array');
-        if (typeof(p2) !== 'object' || length(p2) != 2) throw new Error('p2 is not 1x2 element array');
+        if (typeof(p1) !== 'object' || p1.length != 2) throw new Error('p1 is not 1x2 element array');
+        if (typeof(p2) !== 'object' || p2.length != 2) throw new Error('p2 is not 1x2 element array');
         this.p1 = p1.slice(0);
         this.p2 = p2.slice(0);
     };
